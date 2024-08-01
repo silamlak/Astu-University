9c572f5868f7b90da64502d2ad30a1056d4c6190
fikadu <fikadu026@gmail.com>
2024-08-01 10:48:28 +0300

    u

--- admin/src/pages/EditProfile.jsx
+++ admin/src/pages/EditProfile.jsx
@@ -27,8 +27,11 @@ const EditProfile = () => {
     register,
     handleSubmit,
     formState: { errors },
-    reset
-  } = useForm();
+    reset,
+    watch,
+  } = useForm({
+    mode: "onChange",
+  });
 
   const updateUserPassword = async (newPassword) => {
     console.log("hello");
@@ -41,12 +44,12 @@ const EditProfile = () => {
   const mutation = useMutation({
     mutationFn: (newPassword) => updateUserPassword(newPassword),
     onSuccess: () => {
-reset()
-toast.success(
-  <p className="text-green-500 text-xs italic mt-4">
-    Password updated successfully!
-  </p>
-);
+      reset();
+      toast.success(
+        <p className="text-green-500 text-xs italic mt-4">
+          Password updated successfully!
+        </p>
+      );
     },
     onError: (err) => {
       console.log(err);
@@ -57,74 +60,108 @@ toast.success(
     mutation.mutate(data.password);
   };
 
+  // Watch the password fields to ensure they match
+  const password = watch("password");
+  const confirmPassword = watch("confirmPassword");
+
+  // Check if the passwords match
+  const passwordsMatch = password === confirmPassword;
+
   if (isLoading) {
-    return <div className="flex w-full justify-center"><Loading /></div>;
+    return (
+      <div className="flex w-full justify-center">
+        <Loading />
+      </div>
+    );
   }
 
   if (isError) {
     return <div>Error loading user data</div>;
   }
 
- return (
-   <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900">
-     <div className="w-full max-w-md bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
-       <h1 className="text-2xl mb-4 text-center text-gray-900 dark:text-gray-100">
-         Edit Profile
-       </h1>
-       <form onSubmit={handleSubmit(onSubmit)}>
-         <div className="mb-4">
-           <label
-             className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-             htmlFor="email"
-           >
-             Email
-           </label>
-           <input
-             type="email"
-             id="email"
-             value={user.email}
-             disabled
-             className="shadow bg-gray-50 text-gray-950 appearance-none border rounded w-full py-2 px-3 dark:text-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-           />
-         </div>
-         <div className="mb-4">
-           <label
-             className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
-             htmlFor="password"
-           >
-             New Password
-           </label>
-           <input
-             type="password"
-             id="password"
-             {...register("password", { required: true, minLength: 6 })}
-             className="shadow bg-gray-50 text-gray-950  appearance-none border rounded w-full py-2 px-3 dark:text-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-           />
-           {errors.password && (
-             <p className="text-red-500 text-xs italic">
-               Password is required and must be at least 6 characters long.
-             </p>
-           )}
-         </div>
-         <div className="flex items-center justify-between">
-           <button
-             type="submit"
-             className="flex items-center justify-between gap-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-             disabled={mutation.isPending}
-           >
-             <p>{mutation.isPending ? "Updating..." : "Update Password"}</p>
-             {mutation.isPending && <Loading />}
-           </button>
-         </div>
-       </form>
-       {mutation.isError && (
-         <p className="text-red-500 text-xs italic mt-4">
-           Error updating password. Please try again.
-         </p>
-       )}
-     </div>
-   </div>
- );
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 dark:bg-gray-900">
+      <div className="w-full max-w-md bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl mb-4 text-center text-gray-900 dark:text-gray-100">
+          Edit Profile
+        </h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={user.email}
+              disabled
+              className="shadow bg-gray-50 text-gray-950 appearance-none border rounded w-full py-2 px-3 dark:text-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              New Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              {...register("password", { required: true, minLength: 6 })}
+              className="shadow bg-gray-50 text-gray-950 appearance-none border rounded w-full py-2 px-3 dark:text-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+            {errors.password && (
+              <p className="text-red-500 text-xs italic">
+                Password is required and must be at least 6 characters long.
+              </p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2"
+              htmlFor="confirmPassword"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              {...register("confirmPassword", {
+                required: true,
+                validate: (value) =>
+                  value === password || "Passwords do not match",
+              })}
+              className="shadow bg-gray-50 text-gray-950 appearance-none border rounded w-full py-2 px-3 dark:text-gray-200 dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+            {errors.confirmPassword && (
+              <p className="text-red-500 text-xs italic">
+                {errors.confirmPassword.message}
+              </p>
+            )}
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="flex items-center justify-between gap-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              disabled={mutation.isPending || !passwordsMatch}
+            >
+              <p>{mutation.isPending ? "Updating..." : "Update Password"}</p>
+              {mutation.isPending && <Loading />}
+            </button>
+          </div>
+        </form>
+        {mutation.isError && (
+          <p className="text-red-500 text-xs italic mt-4">
+            Error updating password. Please try again.
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default EditProfile;
